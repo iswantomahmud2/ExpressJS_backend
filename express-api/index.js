ea65d23fbf0fb094a29401d700cbad4945c286f9
iswantomahmud2 <iswantomahmud22@gmail.com>
2024-07-01 15:37:32 +0800

    Insert Data ke Database Express js

--- express-api/index.js
+++ express-api/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//import body parser
+const bodyParser = require('body-parser');
+
+//parser application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+//parse application/json
+app.use(bodyParser.json())
+
+
 //import route posts
 const postRouter = require('./routes/posts');
 app.use('/api/posts', postRouter); //use route posts di Express
