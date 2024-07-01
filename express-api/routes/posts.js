33f6d3a9b45996b33502d6fa25110700ce18f9b7
iswantomahmud2 <iswantomahmud22@gmail.com>
2024-07-01 22:22:33 +0800

    Delete Data dari Database

--- express-api/routes/posts.js
+++ express-api/routes/posts.js
@@ -143,4 +143,27 @@ router.patch('/update/:id', [
     })
 });
 
+/**
+ * Delete Data Post
+ */
+
+router.delete('/delete/(:id)', function (req, res) {
+    let id = req.params.id
+
+    connection.query(`DELETE FROM posts WHERE id = ${id}`, function (err, rows) {
+        //if(err) throw err
+        if (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Internal Server Error'
+            })
+        } else {
+            return res.status(200).json({
+                status: true,
+                message: 'Delete Data Successfully!'
+            })
+        }
+    })
+});
+
 module.exports = router;
